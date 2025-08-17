07e2ba20fb7fe50dd6d3c49f9fd683cea46367f9
wozhoo6 <warrentabora3410@gmail.com>
2025-08-17 21:34:21 +0800

    analystics and icon update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './astro.jpg';
 import './App.css';
+import Home from './pages/home';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main">
+        <Home />
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2025 AstroKeyISP. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/home';
+import { Analytics } from '@vercel/analytics/react'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
+    <Analytics />
   </React.StrictMode>
 );
 
